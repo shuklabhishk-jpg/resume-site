a8fdf471ff0535da3b54e75fa1267b8ebd386c12
shuklabhishk-jpg <shuklabhishk@gmail.com>
2026-06-07 18:43:00 +0530

    generate.js

--- api/generate.js
+++ api/generate.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
   const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${GEMINI_API_KEY}`,
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
